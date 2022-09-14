a3a80764a1e308ead260c7311724d037d4ceb56c
johanneslil <jocka.liljedahl@gmail.com>
2022-09-15 00:46:29 +0200

    fix: reverse tuple duplicates removed

--- round_robin.py
+++ round_robin.py
@@ -11,8 +11,13 @@ def match_order(players: list[int]) -> MatchList:
     :param players: list of players that should play matches
     :return: list of unique random player combinations
     """
-    match_list = [(p1, p2) for p1 in players for p2 in players if p1 != p2]
+    match_list = []
+    for p1 in players:
+        for p2 in players:
+            if (p1 != p2 and ((p1, p2) not in (match_list)) and ((p2, p1) not in (match_list))):
+                match_list.append((p1, p2))
     shuffle(match_list)
+
     return match_list
 
 
@@ -25,6 +30,8 @@ def test_no_duplicate_matches():
     for number_of_players in range(min_players, max_players):
         match_list = match_order(range(number_of_players))
         assert len(match_list) == len(set(match_list))
+        assert len(match_list) == len(
+            set([match[::-1] for match in match_list]))
 
 
 def test_all_play_all():
@@ -32,8 +39,10 @@ def test_all_play_all():
         match_list = match_order(range(number_of_players))
 
         # compare generated number of matches to theoretical number of matches
-        assert len(match_list) == (number_of_players * (number_of_players-1))
+        assert len(match_list) == number_of_players*(number_of_players-1)/2
 
 
 test_no_duplicate_matches()
 test_all_play_all()
+
+print(match_order(range(3)))
