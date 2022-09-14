d567f17ad5bd1150b98954fd19833db11ddfd5bb
johanneslil <jocka.liljedahl@gmail.com>
2022-09-14 23:38:41 +0200

    fix shuffle + add tests

--- round_robin.py
+++ round_robin.py
@@ -1,5 +1,7 @@
 from random import shuffle
 
+from numpy import number
+
 MatchList = list[tuple[int, int]]
 
 
@@ -12,4 +14,28 @@ def match_order(players: list[int]) -> MatchList:
     :return: list of unique random player combinations
     """
     match_list = [(p1, p2) for p1 in players for p2 in players if p1 != p2]
-    return shuffle(match_list)
+    shuffle(match_list)
+    return match_list
+
+
+# temporary testing, allowed number of players is 3-8
+min_players = 3
+max_players = 8
+
+
+def test_no_duplicate_matches():
+    for number_of_players in range(min_players, max_players):
+        match_list = match_order(range(number_of_players))
+        assert len(match_list) == len(set(match_list))
+
+
+def test_all_play_all():
+    for number_of_players in range(min_players, max_players):
+        match_list = match_order(range(number_of_players))
+
+        # compare generated number of matches to theoretical number of matches
+        assert len(match_list) == (number_of_players * (number_of_players-1))
+
+
+test_no_duplicate_matches()
+test_all_play_all()
