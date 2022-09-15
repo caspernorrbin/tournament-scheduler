ad7ebf7b92edfdc4ba82c43e2818fb78656bc46f
Kipav <55412120+Kipav@users.noreply.github.com>
2022-09-15 13:30:47 +0200

    Added functions select_player and between_matches

--- game.py
+++ game.py
@@ -1,3 +1,36 @@
+## Takes a list of players and returns a selected player or None if selection is cancelled
+def select_player(players):
+    players.insert(0, "[Go back]")
+    while True:
+        for i in range(len(players)):
+            print(f"{i+1}. {players[i]}")
+        selection = input("Selection: ")
+        if selection == "1":
+            return None
+        elif selection.isdigit() and int(selection) <= len(players) and int(selection) > 0:
+            return players[int(selection)-1]
+        else:
+            print("Invalid selection")
+
+## Takes a list of players and the players of the upcoming match.
+## Returns a list of players who want to quit before the upcoming match.
+def between_matches(players, player1, player2):
+    quitters = []
+    while True:
+        #print("Leaderboard TODO")
+        print(f"Next match: {player1} vs {player2}")
+        print("1. Continue")
+        print("2. A player wants to quit")
+        selection = input("Selection: ")
+        if selection == "1":
+            return quitters
+        elif selection == "2":
+            quitter = select_player(players)
+            if quitter != None:
+                quitters.append(quitter)
+            if quitter == player1 or quitter == player2:
+                return quitters
+
 def startmenu():
     print('''Welcome to the game!
 [S]tart game]
