dc4140711567382374a0111da057532d9ab39984
Oskar Pettersson Löfstedt <00oskpet@gmail.com>
2022-09-16 11:49:38 +0200

    Renamed testcases for consistency, added testcases for maximum board size

--- test_game_state.py
+++ test_game_state.py
@@ -1,23 +1,38 @@
 import unittest as ut
 import game_state
 
-class BoardCannotBeSmallerThanSixBySix(ut.TestCase):
-    
-    def test_width_cannot_be_less_than_six(self):
+
+class BoardCannotBeSmallerThan6x6(ut.TestCase):
+
+    def test_width_cannot_be_less_than_6(self):
         with self.assertRaises(ValueError):
             game_state.Board(5, 10)
 
-
-    def test_width_can_be_equal_to_six(self):
+    def test_width_can_be_equal_to_6(self):
         game_state.Board(6, 10)
 
-    def test_heigth_cannot_be_less_than_six(self):
+    def test_heigth_cannot_be_less_than_6(self):
         with self.assertRaises(ValueError):
             game_state.Board(10, 5)
 
-    def test_height_can_be_equal_to_six(self):
+    def test_height_can_be_equal_to_6(self):
         game_state.Board(10, 6)
 
+
+class BoardCannotBeLargerThan24x24(ut.TestCase):
+    def test_width_cannot_be_more_than_24(self):
+        with self.assertRaises(ValueError):
+            game_state.Board(25, 10)
+
+    def test_width_can_be_equal_to_24(self):
+        game_state.Board(24, 10)
+
+    def test_heigth_cannot_be_less_more_24(self):
+        with self.assertRaises(ValueError):
+            game_state.Board(10, 25)
+
+    def test_height_can_be_equal_to_24(self):
+        game_state.Board(10, 24)
 
 
 if __name__ == '__main__':
