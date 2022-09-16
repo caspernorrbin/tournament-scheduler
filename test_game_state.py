a27fd2b255c0af68a1472993243c493ecb2ad439
Oskar Pettersson Löfstedt <00oskpet@gmail.com>
2022-09-16 11:35:10 +0200

    Renamed test cases and test file. Set verbosity of tests to 2.

--- test_game_state.py
+++ test_game_state.py
@@ -4,22 +4,22 @@ import game_state
 
 class BoardCannotBeSmallerThanSixBySix(ut.TestCase):
     
-    def width_cannot_be_less_than_six(self):
+    def test_width_cannot_be_less_than_six(self):
         with self.assertRaises(ValueError):
             game_state.Board(5, 10)
 
 
-    def width_can_be_equal_to_six(self):
+    def test_width_can_be_equal_to_six(self):
         game_state.Board(6, 10)
 
-    def heigth_cannot_be_less_than_six(self):
+    def test_heigth_cannot_be_less_than_six(self):
         with self.assertRaises(ValueError):
             game_state.Board(10, 5)
 
-    def height_can_be_equal_to_six(self):
+    def test_height_can_be_equal_to_six(self):
         game_state.Board(10, 6)
 
 
 
 if __name__ == '__main__':
-    ut.main()
+    ut.main(verbosity=2)
