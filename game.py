fe0743be7ecafb3d2ba5ea36e8a03cf957f05a3e
rebfal <73636061+rebfal@users.noreply.github.com>
2022-09-17 16:00:43 +0200

    tournament initialisation with player objects with names and id

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import uuid 
+
 def startmenu():
     print('''Welcome to the game!
 [S]tart game]
@@ -15,6 +17,27 @@ def game():
 
 def tournament():
     print("Tournament")
+
+# created a function to start the tournament (we did not user the first function in case anyone is working on tht one)
+def tournament_test():
+    # get all the players in a list, the players are obbjects in the Player class
+    player_list = get_player_list()
+    # start tournament with the player_list
+    tournament = Tournament(player_list)
+    
+def get_player_list():
+    player_list = []
+    # gets the names as an array from the function get_player_names
+    names = get_player_names()
+
+    for name in names:
+        # assing each player a unique id with the uuid4 function
+        player_id = uuid.uuid4().hex
+        # create a new Player object for each player with their names and UUID
+        player = Player(name, player_id)
+        player_list.append(player)
+    return player_list
+
 
 
 def main():
