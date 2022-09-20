0827c8b21e062c26484d5f70ba0b17365da74781
caspernorrbin <caspernorrbin1@gmail.com>
2022-09-20 15:56:31 +0200

    corrected formatting

--- match.py
+++ match.py
@@ -1,12 +1,12 @@
 from enum import Enum
 
-class Match_result(Enum):
+class MatchResult(Enum):
     P1_WIN = 1
     P2_WIN = 2
     P1_QUIT = 3
     P2_QUIT = 4
 
-def play_match(Player1, Player2):
+def play_match(player1, player2):
     """
     Simulates a match between two players
     :param Player1: the first player
@@ -14,7 +14,7 @@ def play_match(Player1, Player2):
     :return: 
     """
     print("Playing match...")
-    print("Player 1: " + Player1.name)
-    print("Player 2: " + Player2.name)
+    print("Player 1: " + player1.name)
+    print("Player 2: " + player2.name)
 
-    return Match_result.P1_WIN
+    return MatchResult.P1_WIN
