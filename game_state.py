1caa5842059fb04a2a6cf92ace212a3feb648a19
00oskpet <42684085+00oskpet@users.noreply.github.com>
2022-09-23 11:32:02 +0200

    Feature/game state (#10)
    * Added Board class for storing board state
    
    * Added Piece class, type hints and add_piece operation.
    
    * Added test case for minimum board size.
    
    * Renamed test cases and test file. Set verbosity of tests to 2.
    
    * Removed autoadded ast import
    
    * Renamed testcases for consistency, added testcases for maximum board size
    
    * Board is now square

---
+++ game_state.py
@@ -0,0 +1,62 @@
+from dataclasses import dataclass
+from collections import defaultdict
+from enum import Enum
+
+MIN_BOARD_SIZE = 6
+MAX_BOARD_SIZE = 24
+
+
+class Orientation(Enum):
+    HORIZONTAL = 0
+    VERTICAL = 1
+
+
+class Color(Enum):
+    BLACK = 0
+    WHITE = 1
+
+
+@dataclass
+class Piece:
+    orientation: Orientation
+    color: Color
+
+
+@dataclass(init=False)
+class Board:
+    """Data structure for representing a board. Currently implemented as a dict 
+    of coordinates for piece placement to a list of pieces.
+
+    TODO: It might be probable that other components represent their board as a
+    3D list, if so this representation might need to change. Representing the
+    board as a dict of list should have the benefit of being easlily 
+    serializable."""
+    size: int
+
+    board: defaultdict[tuple: list]
+
+    def __init__(self, size: int) -> None:
+        """TODO: If board input takes care of checking for valid sizes, the
+        checks here become unneccesary. It is currently impossible to construct
+        a board of an invalid size."""
+        if not MIN_BOARD_SIZE <= size <= MAX_BOARD_SIZE:
+            raise ValueError(
+                f'Dimension of board is ({size},{size}) side length must be between {MIN_BOARD_SIZE} and {MAX_BOARD_SIZE}')
+        
+        self.size = size
+
+        self.board = defaultdict(default_factory=list)
+
+    @property
+    def dimensions(self):
+        return (self.size, self.size)
+
+
+    def add_piece(self, piece: Piece, coordinate: tuple[int, int]) -> None:
+        location = self.board[coordinate]
+        
+        # If the top piece is vertical we cannot place a piece there.
+        if location and location[0].orientation == Orientation.VERTICAL:
+            return
+        
+        self.board[location].push(piece)
