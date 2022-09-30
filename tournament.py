175fe157f60323ae054402ad652ed840dfb9c767
Kipav <55412120+Kipav@users.noreply.github.com>
2022-09-30 10:41:37 +0200

    added whiteplays increment

--- tournament.py
+++ tournament.py
@@ -122,6 +122,7 @@ class Tournament:
         if white.whiteplays < black.whiteplays:
             white, black = black, white
         result = play_match(white, black)
+        white.whiteplays += 1
         if MatchResult.WHITE_WIN == result:
             self.update_leaderboard(white.player_id)
         elif MatchResult.BLACK_WIN == result:
