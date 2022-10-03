523761e9594ccf0da94c03fe1933bdc7090aa492
Oskar Pettersson Löfstedt <00oskpet@gmail.com>
2022-10-03 11:18:04 +0200

    Separated state and serialization

--- test_game_state.py
+++ test_game_state.py
@@ -16,33 +16,7 @@ class BoardSizeBetween6x6And24x24(ut.TestCase):
             game_state.Board(26)
 
     def test_size_can_be_equal_to_24(self):
-        game_state.Board(24)
-
-class SerializationOfGameState(ut.TestCase):
-
-    def setUp(self) -> None:
-        self.board = game_state.Board(6)
-        a = game_state.Piece(game_state.Orientation.HORIZONTAL, game_state.Color.WHITE)
-        b = game_state.Piece(game_state.Orientation.HORIZONTAL, game_state.Color.BLACK)
-        c = game_state.Piece(game_state.Orientation.VERTICAL, game_state.Color.WHITE)
-
-        self.board.add_piece(a, (1,1))
-        self.board.add_piece(b, (1,1))
-        self.board.add_piece(c, (0,0))
-
-        game_state.save_game_state(self.board)
-
-    def test_saving_game_state(self):
-        game_state.save_game_state(self.board)
-
-    def test_loading_game_state(self):
-        state = game_state.load_game_state()
-        self.assertEqual(type(self.board), type(state))
-
-    def test_saving_and_loading_does_not_change_state(self):
-        state = game_state.load_game_state()
-        self.assertEqual(self.board, state)
-        
+        game_state.Board(24)        
 
 if __name__ == '__main__':
     ut.main(verbosity=2)
